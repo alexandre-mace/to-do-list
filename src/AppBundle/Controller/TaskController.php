36b187b1e5eb047a6aaf93d4fe41d7e852924af7
alexandre-mace <contact.alexandremace@gmail.com>
2018-10-18 19:53:14 +0200

    added handler services + front styling

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -7,33 +7,33 @@ use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Service\TaskSessionHandler;
 
 class TaskController extends Controller
 {
     /**
      * @Route("/", name="task_list")
      */
-    public function listAction(Request $request)
+    public function listAction(Request $request, TaskSessionHandler $taskSessionHandler)
     {
         return $this->render('task/list.html.twig', [
-            'tasks' => $this->get('session')->get('tasks'),
+            'tasks' => $taskSessionHandler->loadAll(),
         ]);
     }
 
     /**
      * @Route("/add", name="task_add")
      */
-    public function addAction(Request $request)
+    public function addAction(Request $request, TaskSessionHandler $taskSessionHandler)
     {
         if ($request->getMethod() === "POST") {
             $username = $request->request->get('username');
             $title = $request->request->get('title');
             $description = $request->request->get('description');
-
-            $tasks = $this->get('session')->get('tasks');
+            $tasks = $taskSessionHandler->loadAll();
             $tasks[] = new Task($title, $description, false, new User($username));
 
-            $this->get('session')->set('tasks', $tasks);
+            $taskSessionHandler->setTasks($tasks);
             return $this->redirectToRoute('task_list');
         }
         return $this->render('task/add.html.twig', [
@@ -44,33 +44,20 @@ class TaskController extends Controller
     /**
      * @Route("/update/{id}", name="task_update")
      */
-    public function updateAction(Request $request, $id)
+    public function updateAction(Request $request, $id, TaskSessionHandler $taskSessionHandler)
     {
-        foreach ($this->get('session')->get('tasks') as $key => $value) {
-            if ($value->getId() == $id) {
-                if ($value->getComplete()) {
-                    $value->setComplete(false);
-                } else {
-                    $value->setComplete(true);
-
-                }
-            }
-        }
+        $task = $taskSessionHandler->loadOne($id);
+        $task->setComplete(!$task->getComplete());
         return $this->redirectToRoute('task_list');
     }
 
     /**
      * @Route("/delete/{id}", name="task_delete")
      */
-    public function deleteAction(Request $request, $id)
+    public function deleteAction(Request $request, $id, TaskSessionHandler $taskSessionHandler)
     {
-        $tasks = array();
-        foreach ($this->get('session')->get('tasks') as $key => $value) {
-            if ($value->getId() !== $id) {
-                $tasks[] = $value;
-            }
-        }
-        $this->get('session')->set('tasks', $tasks);
+        $newTasks = $taskSessionHandler->deleteOne($id);
+        $taskSessionHandler->setTasks($newTasks);
         return $this->redirectToRoute('task_list');
     }
 }
---
+++ var/cache/dev/twig/15/159eceb31676c8062d075c5084292bc91e0ef76a30ee18a94f05a33c727faebd.php
@@ -0,0 +1,143 @@
+<?php
+
+/* task/add.html.twig */
+class __TwigTemplate_37e195f4d3e21140739c155138cf814f320e3557ceac4cf5956b3f622fe3c15d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("base.html.twig", "task/add.html.twig", 1);
+        $this->blocks = array(
+            'header' => array($this, 'block_header'),
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "task/add.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "task/add.html.twig"));
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 3
+    public function block_header($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "header"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "header"));
+
+        // line 4
+        echo "    <div class=\"jumbotron\">
+        <h1 class=\"display-4\">Adding a new task to the to do list!</h1>
+        <p class=\"lead\">Fill in the form bellow.</p>
+        <hr class=\"my-4\">
+    </div>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 11
+    public function block_body($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        // line 12
+        echo "    <form method=\"POST\" id=\"taskForm\">
+        <label for=\"username\">Your name</label>
+        <input class=\"form-control\" type=\"text\" placeholder=\"Your name\" name=\"username\">
+        <label for=\"title\">Task name</label>
+        <input class=\"form-control\" type=\"text\" placeholder=\"Task name\" name=\"title\">
+        <div class=\"form-group\">
+            <label for=\"description\">Task Description</label>
+            <textarea class=\"form-control\" id=\"exampleFormControlTextarea1\" rows=\"3\" name=\"description\"></textarea>
+        </div>
+        <button class=\"btn btn-success\" type=\"submit\"><i class=\"fas fa-plus fa-2x\"></i></button>
+    </form>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "task/add.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  74 => 12,  65 => 11,  50 => 4,  41 => 3,  11 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% extends 'base.html.twig' %}
+
+{% block header %}
+    <div class=\"jumbotron\">
+        <h1 class=\"display-4\">Adding a new task to the to do list!</h1>
+        <p class=\"lead\">Fill in the form bellow.</p>
+        <hr class=\"my-4\">
+    </div>
+{% endblock %}
+
+{% block body %}
+    <form method=\"POST\" id=\"taskForm\">
+        <label for=\"username\">Your name</label>
+        <input class=\"form-control\" type=\"text\" placeholder=\"Your name\" name=\"username\">
+        <label for=\"title\">Task name</label>
+        <input class=\"form-control\" type=\"text\" placeholder=\"Task name\" name=\"title\">
+        <div class=\"form-group\">
+            <label for=\"description\">Task Description</label>
+            <textarea class=\"form-control\" id=\"exampleFormControlTextarea1\" rows=\"3\" name=\"description\"></textarea>
+        </div>
+        <button class=\"btn btn-success\" type=\"submit\"><i class=\"fas fa-plus fa-2x\"></i></button>
+    </form>
+{% endblock %}", "task/add.html.twig", "/home/alex/Bureau/taskSymfony/app/Resources/views/task/add.html.twig");
+    }
+}
