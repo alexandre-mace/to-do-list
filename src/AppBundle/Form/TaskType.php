f833d1f0b52fa116c85396426f29a0c57cb78e34
alexandre-mace <contact.alexandremace@gmail.com>
2018-11-02 20:03:38 +0100

    minor changes

--- src/AppBundle/Form/TaskType.php
+++ src/AppBundle/Form/TaskType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class TaskType extends AbstractType
 {
@@ -13,7 +14,7 @@ class TaskType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')->add('description')->add('complete')->add('author')->add('save', SubmitType::class);
+        $builder->add('title')->add('description')->add('complete')->add('author');
     }/**
      * {@inheritdoc}
      */
