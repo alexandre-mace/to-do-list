718bfaa40d0fff8cbba3ebf8fd14da8a832a2efa
alexandre-mace <contact.alexandremace@gmail.com>
2018-11-28 02:25:23 +0100

    updated script js

--- web/js/script.js
+++ web/js/script.js
@@ -2,20 +2,20 @@ $(function () {
 	// SORT BY POSITION
 	$("#sortable").sortable({
 		start: function (event, ui) {
-			var startPosition = ui.item.index();
-			ui.item.data("startPosition", startPosition);
+			var start_position = ui.item.index();
+			ui.item.data('start_position', start_position);
 		},
 		update: function (event, ui) {
 			let movement = ui.position.top - ui.originalPosition.top > 0 ? "down" : "up";
-			let url = ui.item.data("url");
-			let endPosition = ui.item.index();
-			let startPosition = ui.item.data("startPosition");
+			let url = ui.item.data('url');
+			let end_position = ui.item.index();
+			let start_position = ui.item.data('start_position');
 			$.ajax({
 				type: "POST",
 				url: url,
 				data: {
-					endPosition: endPosition,
-					startPosition: startPosition,
+					end_position: end_position,
+					start_position: start_position,
 					movement: movement
 				},
 				dataType: "json",
